dae1f2b35cbda5b895caf0ebe9f956386b9a5f87
Armel Meledje <meledjearmel@gmail.com>
2020-07-27 00:10:44 +0000

    Add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $user = User::create([
+            'name' => 'admin',
+            'lastname' => 'admin',
+            'email' => 'admin@admin.fr',
+            'username' => 'admin',
+            'password' => Hash::make('stockgestion'),
+            'level' => 1,
+            'sex' => 'Masculin',
+            'contact' => '(225) 55-555-555',
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+        Role::create(['name' => 'manager']);
+        Role::create(['name' => 'seller']);
+
+        $user->assignRole($role);
     }
 }
